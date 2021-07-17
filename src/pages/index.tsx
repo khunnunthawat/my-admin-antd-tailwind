c02ec53c2c346a95ed480754ff80575bcca2154f
khunnunthawat <khunnunthawat@gmail.com>
2021-07-18 02:59:20 +0700

    edit-index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import { Button, DatePicker, Typography, version } from 'antd';
+import { Typography } from 'antd';
 
 const { Title } = Typography;
 
@@ -7,16 +7,14 @@ export default function Home() {
   return (
     <div className='flex flex-col items-center justify-center min-h-screen'>
       <Head>
-        <title>Hello Next App</title>
+        <title>My Admin Eiffel</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
       <div className='flex items-center justify-center w-full text-center space-x-4'>
         <Title className='text-pink-500' level={4}>
-          Hello, Developer
+          Admin, Eiffel
         </Title>
-        <DatePicker />
-        <Button type='primary'>Primary Button</Button>
       </div>
     </div>
   );
