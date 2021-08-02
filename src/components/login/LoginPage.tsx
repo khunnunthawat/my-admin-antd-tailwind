b8a8ce44c7e97fdbc9e9f0b8e438e384748dc93f
khunnunthawat <khunnunthawat@gmail.com>
2021-08-02 22:45:33 +0700

    remove-comment

--- src/components/login/LoginPage.tsx
+++ src/components/login/LoginPage.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
 import { Typography, Form, Row, Button, Input } from 'antd';
-// import { ContentLayout } from '@dtstack/shared/backoffice-main-layout';
 
 const { Text } = Typography;
 
 export const LoginPage = () => {
   return (
     <>
-      <div className='flex flex-col items-center justify-center min-h-screen bg-gray-100'>
+      <div className='flex flex-col items-center justify-center p-4 '>
         <div>
-          <div className='flex items-center justify-center space-x-4 bg-white w-80 h-80 p-10 shadow-lg'>
+          <div className='flex items-center justify-center bg-white w-80 h-80 p-10 shadow-lg'>
             <Form>
-              <Form.Item
-                name='username'
-                hasFeedback
-              >
+              <Form.Item name='username' hasFeedback>
                 <Input placeholder={`Username`} />
               </Form.Item>
-              <Form.Item
-                name='password'
-                hasFeedback
-              >
+              <Form.Item name='password' hasFeedback>
                 <Input type='password' placeholder={`Password`} />
               </Form.Item>
               <Row>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,23 @@
 import Head from 'next/head';
 import { Typography } from 'antd';
 import { LoginPage } from '@/components/login/LoginPage';
-import { TestPage } from '../components/login/TestPage';
 
 const { Title } = Typography;
 
 export default function Home() {
   return (
-    // <div className='flex flex-col items-center justify-center min-h-screen'>
-    //   <Head>
-    //     <title>My Admin Eiffel</title>
-    //     <link rel='icon' href='/favicon.ico' />
-    //   </Head>
+    <div className='flex flex-col items-center justify-center min-h-screen'>
+      <Head>
+        <title>My Admin Eiffel</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
 
-    //   <div className='flex items-center justify-center w-full text-center space-x-4'>
-    //     <Title className='text-pink-500' level={4}>
-    //       Admin, Eiffel
-    //     </Title>
-    //   </div>
-    // </div>
-    <LoginPage />
-    // <TestPage />
+      <div className='flex items-center justify-center w-full text-center space-x-8'>
+        <Title className='text-pink-500' level={4}>
+          Admin, Eiffel
+          <LoginPage />
+        </Title>
+      </div>
+    </div>
   );
 }
